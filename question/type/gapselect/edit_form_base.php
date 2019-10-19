50cf6329b9a87b80d6116f11f6eba280f044c5c8
BigBossbeep <56760239+BigBossbeep@users.noreply.github.com>
2019-10-19 17:17:53 +0500

    Update edit_form_base.php

--- question/type/gapselect/edit_form_base.php
+++ question/type/gapselect/edit_form_base.php
@@ -168,8 +168,10 @@ class qtype_gapselect_edit_form_base extends question_edit_form {
      *
      * @return int the maximum group number.
      */
-    function get_maximum_choice_group_number() {
-        return 8;
+    function get_maximum_choice_group_number(const MAX_GROUPS = 15;) {
+        return 15;
+        
+
     }
 
     /**
